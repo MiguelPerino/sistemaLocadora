6230da03ce04f2409fc5097f4e174d967facc512
MiguelPerino <miguelcperino@gmail.com>
2025-12-02 14:38:27 -0300

    First commit

--- sis/classes/doctor.py
+++
@@ -1,9 +0,0 @@
-class Doctor:
-
-    def __init__(self, name, specialty):
-        self.name = name
-        self.specialty = specialty.lower()
-    
-    def __str__(self):
-        return f'Doctor {self.name} | {self.specialty}'
-        
